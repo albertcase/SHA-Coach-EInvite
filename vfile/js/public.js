cc860c04559950f9639c6466eef017eab135512f
Vincen's <vincen.wen@samesamechina.com>
2016-10-17 11:45:01 +0800

    no message

--- vfile/js/public.js
+++ vfile/js/public.js
@@ -76,7 +76,9 @@ var pfun = {
         this.wxshareFun();
     },
     wxshareFun: function(){  //分享信息重置函数
+        alert(6);
         wx.ready(function () {
+            alert(shareArr._title);
             // 在这里调用 API
             // 2. 分享接口
             // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
