9b8583f4fcd9527320afca9e8d916eb6ea2e2008
Vincen's <vincen.wen@samesamechina.com>
2016-10-18 10:43:27 +0800

    no message

--- vfile/js/public.js
+++ vfile/js/public.js
@@ -1,7 +1,7 @@
 var shareArr = {
     "_title": '“品革新·致匠心”VIP鉴赏会邀请函', //分享标题
-    "_desc": "诚邀您莅临“品革新·致匠心”VIP鉴赏会邀请函",    // 分享朋友圈的描述
-    "_desc_friend": "诚邀您莅临“品革新·致匠心”VIP鉴赏会邀请函",    // 分享好友的描述
+    "_desc": "诚邀您莅临COACH“品革新·致匠心”VIP鉴赏会",    // 分享朋友圈的描述
+    "_desc_friend": "诚邀您莅临COACH“品革新·致匠心”VIP鉴赏会",    // 分享好友的描述
     "_link": window.location.href,    //分享的连接
     "_imgUrl": "http://" + window.location.host + "/vfile/img/share.png",   //分享的图片
     "_url": encodeURIComponent(window.location)//encodeURIComponent(window.location.href.split("#")[0]) //.replace('http%3A%2F%2F','')
