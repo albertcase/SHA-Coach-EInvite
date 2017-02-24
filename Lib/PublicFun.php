e720c370558390d0f1167358a1b8d329d38a6f72
Dirc Wang <dirk.wang@samesamechina.com>
2017-02-24 18:45:16 +0800

    update3

--- Lib/PublicFun.php
+++ Lib/PublicFun.php
@@ -8,7 +8,7 @@ class PublicFun {
     return FALSE;
   $url = "http://coach.samesamechina.com/v2/wx/users/{$openid}?access_token=zcBpBLWyAFy6xs3e7HeMPL9zWrd7Xy";
   $userinfo = file_get_contents($url);
-  $userinfo = json_decode($userinfo);
+  $userinfo = json_decode($userinfo, true);
   if(isset($userinfo['code']) && $userinfo['code'] == '200'){
     if(isset($userinfo['data']) && isset($userinfo['data']['subscribe']) && $userinfo['data']['subscribe'])
       return FALSE;
