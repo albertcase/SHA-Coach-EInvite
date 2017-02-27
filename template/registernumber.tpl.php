f5613b8fb20adb7983c25ccacb0e53f8cb57a6db
vincenwen <vincen.wen@samesamechina.com>
2017-02-27 15:16:15 +0800

    xian version submit;

--- template/registernumber.tpl.php
+++ template/registernumber.tpl.php
@@ -207,7 +207,7 @@ top: 30%; z-index: 11; width: 100%;
 	function submitCallback(data){
 
 	    if(data.code == "10" || data.code == "6"){
-	    	window.location.href = "/"
+	    	window.location.reload();
 	    }else{
 			if(data.code != "8"){
 				coachEinviteMethod.count--;
